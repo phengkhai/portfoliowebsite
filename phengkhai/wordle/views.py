163c18dd672dd40cfe7b82effef7e00c7297b0d3
svenkneegear <phengkhai@gmail.com>
2022-05-31 13:50:01 +0800

    work in progress

--- phengkhai/wordle/views.py
+++ phengkhai/wordle/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
-from django.http import HttpResponse
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
 
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
