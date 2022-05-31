c1806d8a411abbefd815c6dca8ba19903a24f231
svenkneegear <phengkhai@gmail.com>
2022-05-31 11:30:42 +0800

    front

--- phengkhai/homepage/views.py
+++ phengkhai/homepage/views.py
@@ -1,4 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import ContactForm
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
 
 def index(request):
-    return render(request,"base.html")
+	if request.method == 'POST':
+		form = ContactForm(request.POST)
+		if form.is_valid():
+			subject = "Website Inquiry" 
+			body = {
+			'organization_name': form.cleaned_data['organization_name'], 
+			'position': form.cleaned_data['position'], 
+			'email': form.cleaned_data['email_address'], 
+			'message':form.cleaned_data['message'], 
+			}
+			message = "\n".join(body.values())
+
+			try:
+				send_mail(subject, message, 'phengkhai@gmail.com', ['phengkhai@gmail.com']) 
+			except BadHeaderError:
+				return HttpResponse('Invalid header found.')
+			return redirect ("main:homepage")
+      
+	form = ContactForm()
+	return render(request, "base.html", {'form':form})
+
+def contact(request):
+	if request.method == 'POST':
+		form = ContactForm(request.POST)
+		if form.is_valid():
+			subject = "Website Inquiry" 
+			body = {
+			'organization_name': form.cleaned_data['organization_name'], 
+			'position': form.cleaned_data['position'], 
+			'email': form.cleaned_data['email_address'], 
+			'message':form.cleaned_data['message'], 
+			}
+			message = "\n".join(body.values())
+
+			try:
+				send_mail(subject, message, 'phengkhai@gmail.com', ['phengkhai@gmail.com']) 
+			except BadHeaderError:
+				return HttpResponse('Invalid header found.')
+			return redirect ("main:homepage")
+      
+	form = ContactForm()
+	return render(request, "base.html", {'form':form})
