a7434fd6b29b8917db2b69c10b4dfbe4613cd357
svenkneegear <phengkhai@gmail.com>
2022-06-02 15:23:55 +0800

    heroku2

--- phengkhai/phengkhai/settings.py
+++ phengkhai/phengkhai/settings.py
@@ -126,6 +126,7 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,  'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/images/' 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
