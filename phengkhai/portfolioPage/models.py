501eefd68bc21d71d61e0cacae0373e07719ff10
svenkneegear <phengkhai@gmail.com>
2022-07-23 00:25:39 +0800

    yes

--- phengkhai/portfolioPage/models.py
+++ phengkhai/portfolioPage/models.py
@@ -26,7 +26,7 @@ class Projects(models.Model):
     desc = RichTextField()
     created_on = models.DateTimeField(auto_now_add=True)
     image = models.CharField(max_length=200, unique=True)
-    #link = models.CharField(max_length=200, unique=True,null=True)
+    link = models.CharField(max_length=200, null=True)
     slug = models.SlugField(unique=True, max_length = 130)
     def __str__(self):
         return self.title
